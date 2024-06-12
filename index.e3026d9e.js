b6a8725a72943800bb1234905699bfe817af0684
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-06-12 19:53:20 +0300

    make build

---
+++ index.e3026d9e.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e){document.querySelector(".logo").addEventListener("click",function(){e()})}),t=new Promise(function(e,t){setTimeout(function(){t(Error("Promise timed out"))},3e3)});e.then(function(){var e=document.createElement("div");e.classList.add("message"),e.textContent="Promise was resolved!",document.body.appendChild(e)}),t.catch(function(){var e=document.createElement("div");e.classList.add("message","error-message"),e.textContent="Promise was rejected!",document.body.appendChild(e)});
+//# sourceMappingURL=index.e3026d9e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const promise1 = new Promise((resolve)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Promise timed out"));
+    }, 3000);
+});
+promise1.then(()=>{
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.textContent = "Promise was resolved!";
+    document.body.appendChild(message);
+});
+promise2.catch(()=>{
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("message", "error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
